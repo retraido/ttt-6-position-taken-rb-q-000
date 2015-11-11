baced118cda8ed82c65f1cdb987d091db7142d8e
retraido <enzosarin@gmail.com>
2015-11-11 15:58:03 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+def position_taken?(board, position)
+  if board[position] == "X"
+    then true
+    elsif board[position] == "O"
+      then true
+      else false
+      end
+    end
